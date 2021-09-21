e52d6ba04e445c9c5b2e6dd6a095d22034acf58e
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-09-21 11:13:25 +0300

    Фикс кравлеров, фикс отображения

--- src/main/java/CastleWars/logic/UnitRoom.java
+++ src/main/java/CastleWars/logic/UnitRoom.java
@@ -40,7 +40,7 @@ public class UnitRoom extends Room {
         str.append("\n[gray]").append(cost).append("\n[white]").append(Icon.get(Blocks.plastaniumCompressor)).append(" : ");
 
         if (income < 0) str.append("[crimson]");
-        else if (income > 0) str.append("[lime]");
+        else if (income > 0) str.append("[lime]+");
         else str.append("[gray]");
         str.append(income);
         label = str.toString();
@@ -53,15 +53,9 @@ public class UnitRoom extends Room {
 
         if (type == Type.Attacker) {
             Unit u = unit.spawn(data.player.team(), (data.player.team() == Team.sharded ? blueSpawn.drawx() : shardedSpawn.drawx()) + Mathf.random(-40, 40), (data.player.team() == Team.sharded ? blueSpawn.drawy() : shardedSpawn.drawy()) + Mathf.random(-40, 40));
-            if (unit == UnitTypes.crawler) {
-                u.type = UnitTypes.mono;
-            }
             u.team(data.player.team());
         } else if (data.player.team().core() != null) {
             Unit u = unit.spawn(data.player.team(), data.player.team().core().x + 30, data.player.team().core().y + Mathf.random(-40, 40));
-            if (unit == UnitTypes.crawler) {
-                u.type = UnitTypes.mono;
-            }
             u.team(data.player.team());
         }
     }
