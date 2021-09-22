c0a6bfe47a305b43e01af9f2e28924f4aaf80997
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-09-22 13:29:20 +0300

    Чета намечается

---
+++ src/main/java/CastleWars/logic/DrillRoom.java
@@ -0,0 +1,15 @@
+package CastleWars.logic;
+
+public class DrillRoom extends TurretRoom {
+
+    public DrillRoom(int x, int y) {
+
+    }
+
+    @Override
+    public void update() {
+        if (buyyed && interval.get(0, 60f) && team.core() != null) {
+            team.core().items.add(ItemStack.with(Items.thorium, 8, Items.blastCompound, 4, Items.surgeAlloy, 4, Items.plastanium, 4))
+        }
+    }
+}
